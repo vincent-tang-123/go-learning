1643bf6ecc0278cbd33fbd8c03d937104bcf2f10
唐 <vincent@tangdeMacBook-Pro.local>
2022-04-21 23:00:08 +0800

    空接口的应用

---
+++ src/first/null_interface.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+// 空接口
+// 接口中没有定义任何方法时，该接口就是一个空接口
+// 任意类型都实现了空接口 --》 空接口可以存储任意类型的值
+
+// 空接口一般不需要提前定义
+type xxx interface {
+
+}
+
+
+// 空接口的应该
+// 1。空接口类型作为函数的参数
+// 2 空接口类型可以作为map 的 value
+
+
+func main() {
+
+	var x interface{} // 定义一个空接口类型
+
+	x = "hello"
+	x = 100
+	x = false
+	//fmt.Println(x) // 为什么 Println 作为函数 不管传什么类型 都能打印出来了？ 空接口
+
+	//var m = make(map[string]interface{}, 16) // 定义了一个对象，key类型 string, 值类型为任意
+	//m["name"] = "vincent"
+	//m["age"] = 18
+	//m["hobby"] = []string{"篮球", "足球", "双色球"}
+	//fmt.Println(m)
+	ret, ok := x.(string) // 类型断言,猜的类型不对时 ok=false, ret=string 类型的零值，会返回一个布尔值，
+	if !ok{
+		fmt.Println("不是字符串类型")
+	}else{
+	fmt.Println("是字符串类型", ret)}
+	
+
+	// 使用switch 进行类型断言
+	switch v := x.(type) {
+	case string:
+		fmt.Printf("是字符串类型， value:%v\n", v)
+	case bool:
+		fmt.Printf("是布尔类型, value:%v\n", v)
+	case int:
+		fmt.Printf("是int类型， value：%v\n", v)
+	default:
+		fmt.Printf("猜不到了，value:%v\n", v)
+	}
+
+}
--- src/first/使用值接受接口和使用指针接受接口.go
+++ src/first/使用值接受接口和使用指针接受接口.go
@@ -13,7 +13,7 @@ type person struct {
 	age int
 }
 
-// 使用值接受接口: 类型的值和类型的指针都能狗保存到接口变量中
+//使用值接受接口: 类型的值和类型的指针都能狗保存到接口变量中
 //func (p person) move(){
 //	fmt.Printf("%s谁在跑...\n", p.name)
 //}
@@ -23,20 +23,53 @@ func (p *person) move(){
 	fmt.Printf("%s谁在跑...\n", p.name)
 }
 
+func (p *person) say(){
+	fmt.Printf("%s谁在叫...\n", p.name)
+}
+
+
+
+
+type sayer interface {
+	say()
+}
+
+// 接口的嵌套
+type animal interface {
+	mover
+	sayer
+}
+
+
+// 空接口的定义
+// 空接口是指没有定义任何方法的接口。因此任何礼物ii选哪个都实现了空接口
+// 空接口类型变量可以存储任意类型的变量
+
+
+
 func main() {
-	var m mover
-	p1 := person{ // p1 是person 类型的值
-		name: "小王子",
-		age: 18,
-	}
+	//var m mover
+	////p1 := person{ // p1 是person 类型的值
+	////	name: "小王子",
+	////	age: 18,
+	////}
 	p2 := &person{// p2 是 person 类型的指针
 		name: "娜扎",
 		age: 18,
 
 	}
-	m = p1 // 无法赋值，因为p1是person类型的值没有实现mover接口
-	m.move()
-	m= p2
-	m.move()
-	fmt.Println(m)
+	////m = p1 // 无法赋值，因为p1是person类型的值没有实现mover结偶
+	//m.move()
+	//m= p2
+	//m.move()
+	//fmt.Println(m)
+
+
+	var a animal // 定一个 animal 类型的变量
+
+	a = p2
+	a.move()
+	a.say()
+	fmt.Println(a)
+
 }
