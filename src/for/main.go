ae484d396cac5dc69fd0d7642d960397f43e3a93
唐 <vincent@tangdeMacBook-Pro.local>
2022-05-12 23:23:20 +0800

    流程控制 for/if else

--- src/for/main.go
+++ src/for/main.go
@@ -1,5 +1,47 @@
 package main
 
+import "fmt"
+
+// for 循环
 func main() {
-	$END$
+	for i:=0; i<10; i++ {
+		fmt.Println(i)
+	}
+
+	// 2 省略初识语句，但是必须保留初识语句后面的分号
+	var i = 0
+	for ; i<10 ; i++{
+		fmt.Println(i)
+	}
+
+	// 3 省略初识语句和结束语句
+	var a = 10
+	for a > 0{
+		fmt.Println(a)
+		a--
+	}
+
+	// 4 无限循环
+	//for {
+	//	fmt.Println("hello beijing")
+	//}
+
+	// 5 break 跳出 for 循环
+
+	//for i:=0; i<5; i++{
+	//	fmt.Println(i)
+	//	if i == 3{
+	//		break
+	//	}
+	//}
+
+	// continue 继续下一次循环
+	for i:=0; i<5; i++{
+		if i == 3{
+			continue // 跳过本次循环
+		}
+		fmt.Println(i)
+
+	}
+
 }
--- src/if_else/main.go
+++ src/if_else/main.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
+// if 判断
 func main() {
-	$END$
+	// 1 基本写法
+	//var score = 65
+	//if score >= 90{
+	//	fmt.Println("a")
+	//} else if score >75{
+	//	fmt.Println("b")
+	//}else{
+	//	fmt.Println("c")
+	//}
+
+	// 2 if 判断的特殊方法
+	if score:=65; score >= 90{
+		fmt.Println("a")
+	} else if score >75{
+		fmt.Println("b")
+	}else{
+		fmt.Println("c")
+	}
+
+	scores := 10
+	if scores > 95{
+		fmt.Println(000)
+	}else{
+		fmt.Println(444)
+	}
+
 }
