898b2a228a1cd934280d16a3f0317059a6085703
唐 <vincent@tangdeMacBook-Pro.local>
2022-05-21 23:51:17 +0800

    go  自定义类型 && 类型别名

---
+++ src/about_tye/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// 自定义类型和类型别名示例
+// 自定义类型和类型别名功能作用相同
+
+// 1 自定义类型
+
+// MyInt 基于 int 类型的自定义类型
+type MyInt int
+
+// 2 类型别名
+
+// NewInt int 类型别名
+type NewInt = int
+
+func main() {
+	var i MyInt
+	fmt.Printf("type: %T value:%v\n", i, i) // type: main.MyInt value:0
+
+	var x NewInt
+	fmt.Printf("type: %T value:%v\n", x , x) // type: int value:0
+
+}
