cb440dd9ccbc739e6de8d39b12920038782fb91e
唐 <vincent@tangdeMacBook-Pro.local>
2022-06-03 21:13:33 +0800

    go goroutine

--- src/goroutine_demo/go_routine_gomaxprocs/main.go
+++ src/goroutine_demo/go_routine_gomaxprocs/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+
+// 管理 goroutine
+var wg sync.WaitGroup
+
+
+func a(){
+	for i:=1;i<10;i++{
+		fmt.Println("a", i)
+	}
+	wg.Done()
+}
+
+func b(){
+	for i:=1;i<10;i++{
+		fmt.Println("b", i)
+	}
+	wg.Done()
+}
+
+
 func main() {
-	$END$
+	runtime.GOMAXPROCS(6) // 开启4个核心
+	wg.Add(2) // 添加管理 2个 goroutine
+	go a()
+	go b()
+	wg.Wait()
 }
--- src/goroutine_demo/main.go
+++ src/goroutine_demo/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
-	$END$
+import (
+	"fmt"
+	"sync"
+)
+
+// goroutine demo
+
+// 监听 所有的 goroutine
+var wg sync.WaitGroup
+
+
+func hello(i int){
+	fmt.Println("hello vincent", i)
+	wg.Done()  // 通知 wg 把计数器 -1
+}
+
+func main() {  // 开启一个主 goroutine 去执行 main 函数
+
+	wg.Add(10000)  // 启动一个 goroutine 并登记 计数牌 +1
+
+	// 循环 执行 goroutine
+	for i:=0;i<10000;i++{
+		//wg.Add(1)
+		go hello(i)  // 开启了一个独立的 goroutine 去执行 hello 函数
+
+	}
+
+	fmt.Println("hello main")
+	//time.Sleep(time.Second)
+
+	wg.Wait()  // 等所有小弟都干完活之后收兵   直到计数器为 0 时关闭
 }
